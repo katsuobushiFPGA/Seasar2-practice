95e68a0b0db4c2db955fe9bf752d143d946610fc
horiba.hiroto <suizmelon@gmail.com>
2017-01-30 21:47:33 +0900

    Add S2AOP examples.

--- src/main/java/section2/s2container/aop/Main.java
+++ src/main/java/section2/s2container/aop/Main.java
@@ -7,9 +7,15 @@ public class Main {
 	
 	public static void main(String args[]) {
 		S2Container container = S2ContainerFactory.create("app.dicon");
-		
 		CalcService calc =(CalcService)container.getComponent(CalcService.class);
 		calc.add(1, 2);
 		calc.hello();
-	}	
+		container.destroy();
+
+		S2Container container2 = S2ContainerFactory.create("app.dicon");
+		TraceTest trc =(TraceTest)container2.getComponent(TraceTest.class);
+		trc.print();
+		trc.test();
+		container2.destroy();
+	}
 }
---
+++ src/main/java/section2/s2container/aop/TraceTest.java
@@ -0,0 +1,10 @@
+package section2.s2container.aop;
+
+public class TraceTest {
+	public void print() {
+		System.out.println("hello,world");
+	}
+	public void test() {
+		System.out.println("test,world");		
+	}
+}
